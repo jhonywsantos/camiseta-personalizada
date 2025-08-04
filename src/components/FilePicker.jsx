93f39303f2a038c3520cc7912f1f68e9ecfebfb9
Jhony W. Santos <jhonywictor73@gmail.com>
2025-08-04 03:07:48 -0300

    Update FilePicker.jsx

--- src/components/FilePicker.jsx
+++ src/components/FilePicker.jsx
@@ -13,11 +13,11 @@ const FilePicker = ({ file, setFile, readFile }) => {
           onChange={(e) => setFile(e.target.files[0])}
         />
         <label htmlFor="file-upload" className="filepicker-label">
-          Upload File
+          Fazer Upload
         </label>
 
         <p className="mt-2 text-gray-500 text-xs truncate">
-          {file === "" ? "No file selected" : file.name}
+          {file === "" ? "Nenhum arquivo selecionado" : file.name}
         </p>
       </div>
 
@@ -30,7 +30,7 @@ const FilePicker = ({ file, setFile, readFile }) => {
         />
         <CustomButton
           type="filled"
-          title="Full"
+          title="Inteiro"
           handleClick={() => readFile("full")}
           customStyles="text-xs"
         />
