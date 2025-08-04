2a43ee9da02f17ab494d8e1c68284cb9fd9dba7d
Jhony W. Santos <jhonywictor73@gmail.com>
2025-08-04 03:13:39 -0300

    Added images

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,8 @@ const state = proxy({
   color: '#f8b20eff',
   isLogoTexture: true,
   isFullTexture: false,
-  logoDecal: './threejs.png',
-  fullDecal: './threejs.png',
+  logoDecal: './src/assets/pinkFloyd.png',
+  fullDecal: './src/assets/threejs.png',
 });
 
 export default state;
