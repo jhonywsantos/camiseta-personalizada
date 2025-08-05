c0dee92ddb3d16ce47dac0ba7d5ea4afdf36f33d
Jhony W. Santos <jhonywictor73@gmail.com>
2025-08-04 22:15:49 -0300

    feat(ui): update redesigned images

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,7 @@ import fileIcon from "./file.png";
 import swatch from "./swatch.png";
 import download from "./download.png";
 
-import logoShirt from "./logo-tshirt.png";
-import stylishShirt from "./stylish-tshirt.png";
+import logoShirt from "./CorpseBride.jpeg";
+import stylishShirt from "./CorpseBride.jpeg";
 
 export { ai, fileIcon, swatch, download, logoShirt, stylishShirt };
